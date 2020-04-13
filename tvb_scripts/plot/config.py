e97e6c5cddd69a3c4a365f4d4945b82617199797
bvalean <bogdan.valean@codemart.ro>
2020-04-13 10:04:08 +0300

    HBP-75 Added back to scripts extra configurations

---
+++ tvb_scripts/plot/config.py
@@ -0,0 +1,143 @@
+# -*- coding: utf-8 -*-
+#
+#
+#  TheVirtualBrain-Scientific Package. This package holds all simulators, and
+# analysers necessary to run brain-simulations. You can use it stand alone or
+# in conjunction with TheVirtualBrain-Framework Package. See content of the
+# documentation-folder for more details. See also http://www.thevirtualbrain.org
+#
+# (c) 2012-2020, Baycrest Centre for Geriatric Care ("Baycrest") and others
+#
+# This program is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with this
+# program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#   CITATION:
+# When using The Virtual Brain for scientific publications, please cite it as follows:
+#
+#   Paula Sanz Leon, Stuart A. Knock, M. Marmaduke Woodman, Lia Domide,
+#   Jochen Mersmann, Anthony R. McIntosh, Viktor Jirsa (2013)
+#       The Virtual Brain: a simulator of primate brain network dynamics.
+#   Frontiers in Neuroinformatics (7:10. doi: 10.3389/fninf.2013.00010)
+#
+#
+
+"""
+.. moduleauthor:: Dionysios Perdikis <dionperd@gmail.com>
+.. moduleauthor:: Gabriel Florea <gabriel.florea@codemart.ro>
+"""
+
+import os
+from datetime import datetime
+
+import numpy
+from tvb.simulator.plot.config import FiguresConfig
+
+
+class GenericConfig(object):
+    _module_path = os.path.dirname(__file__)
+
+    # Identify and choose the Simulator, or data folder type to read.
+    MODE_H5 = "H5"
+    MODE_TVB = "TVB"
+
+
+class InputConfig(object):
+    _base_input = os.getcwd()
+
+    @property
+    def HEAD(self):
+        if self._head_folder is not None:
+            return self._head_folder
+
+        # or else, try to find tvb_data module
+        try:
+            import tvb_data
+            return os.path.dirname(tvb_data.__file__)
+        except ImportError:
+            return self._base_input
+
+    @property
+    def IS_TVB_MODE(self):
+        """Identify and choose the Input data type to use"""
+        return self._data_mode == GenericConfig.MODE_TVB
+
+    @property
+    def RAW_DATA_FOLDER(self):
+        if self._raw_data is not None:
+            return self._raw_data
+
+        return os.path.join(self._base_input, "data", "raw")
+
+    def __init__(self, head_folder=None, raw_folder=None, data_mode=GenericConfig.MODE_TVB):
+        self._head_folder = head_folder
+        self._raw_data = raw_folder
+        self._data_mode = data_mode
+
+
+class OutputConfig(object):
+    subfolder = None
+
+    def __init__(self, out_base=None, separate_by_run=False):
+        """
+        :param work_folder: Base folder where logs/figures/results should be kept
+        :param separate_by_run: Set TRUE, when you want logs/results/figures to be in different files / each run
+        """
+        self._out_base = out_base or os.path.join(os.getcwd(), "outputs")
+        self._separate_by_run = separate_by_run
+
+    @property
+    def FOLDER_LOGS(self):
+        folder = os.path.join(self._out_base, "logs")
+        if self._separate_by_run:
+            folder = folder + datetime.strftime(datetime.now(), '%Y-%m-%d_%H-%M')
+        if not (os.path.isdir(folder)):
+            os.makedirs(folder)
+        return folder
+
+    @property
+    def FOLDER_RES(self):
+        folder = os.path.join(self._out_base, "res")
+        if self._separate_by_run:
+            folder = folder + datetime.strftime(datetime.now(), '%Y-%m-%d_%H-%M')
+        if not (os.path.isdir(folder)):
+            os.makedirs(folder)
+        if self.subfolder is not None:
+            folder = os.path.join(folder, self.subfolder)
+        return folder
+
+    @property
+    def FOLDER_TEMP(self):
+        return os.path.join(self._out_base, "temp")
+
+
+class CalculusConfig(object):
+    # Normalization configuration
+    WEIGHTS_NORM_PERCENT = 99
+
+    # If True a plot will be generated to choose the number of eigenvalues to keep
+    INTERACTIVE_ELBOW_POINT = False
+
+    MIN_SINGLE_VALUE = numpy.finfo("single").min
+    MAX_SINGLE_VALUE = numpy.finfo("single").max
+    MAX_INT_VALUE = numpy.iinfo(numpy.int64).max
+    MIN_INT_VALUE = numpy.iinfo(numpy.int64).max
+
+
+class Config(object):
+    generic = GenericConfig()
+    calcul = CalculusConfig()
+
+    def __init__(self, head_folder=None, raw_data_folder=None, output_base=None, separate_by_run=False):
+        self.input = InputConfig(head_folder, raw_data_folder)
+        self.out = OutputConfig(output_base, separate_by_run)
+        self.figures = FiguresConfig(output_base, separate_by_run)
+
+
+CONFIGURED = Config()
